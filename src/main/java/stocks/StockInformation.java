e4a5cf1e87e435e3fc86dd9db84d03b2d42a7853
tanvibhonsle <tanvi.bhonsle@globant.com>
2017-06-12 15:45:00 +0530

    Add files via upload

---
+++ src/main/java/stocks/StockInformation.java
@@ -0,0 +1,63 @@
+package stocks;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by tanvi.bhonsle on 12/06/17.
+ */
+public class StockInformation {
+    private Integer Id;
+    private String stockCode;
+    private BigDecimal currentPrice;
+    private BigDecimal yearHigh;
+    private BigDecimal yearLow;
+    private BigDecimal targetPrice;
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public BigDecimal getYearHigh() {
+        return yearHigh;
+    }
+
+    public void setYearHigh(BigDecimal yearHigh) {
+        this.yearHigh = yearHigh;
+    }
+
+    public BigDecimal getYearLow() {
+        return yearLow;
+    }
+
+    public void setYearLow(BigDecimal yearLow) {
+        this.yearLow = yearLow;
+    }
+
+    public BigDecimal getTargetPrice() {
+        return targetPrice;
+    }
+
+    public void setTargetPrice(BigDecimal targetPrice) {
+        this.targetPrice = targetPrice;
+    }
+}
---
+++ src/main/java/stocks/StocksController.java
@@ -0,0 +1,82 @@
+package stocks;
+
+/**
+ * Created by tanvi.bhonsle on 12/06/17.
+ */
+
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.BeanToCsv;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@RestController
+public class StocksController {
+
+    @RequestMapping(value = "/")
+    public void stocksInformation(Model model) {
+        try {
+            Stream<String> streams = Files.lines(Paths.get("/Users/tanvi.bhonsle/Downloads/Stocks1.txt"));
+
+            String csv = "/Users/tanvi.bhonsle/Downloads/output.csv";
+            CSVWriter writer = new CSVWriter(new FileWriter(csv));
+
+            List<StockInformation> stockData = new ArrayList<>();
+
+            final Integer[] i = {1};
+            streams.forEach(stream -> {
+                StockInformation stockInformation = new StockInformation();
+                stockInformation.setId(i[0]++);
+                stockInformation.setStockCode(stream);
+                Stock stock = null;
+                try {
+                    stock = YahooFinance.get(stream);
+                } catch (IOException e) {
+                    System.out.print("Error while fetching finance data");
+                }
+                BigDecimal price = stock.getQuote().getPrice();
+                stockInformation.setCurrentPrice(price);
+                BigDecimal yearHigh = stock.getQuote().getYearHigh();
+                stockInformation.setYearHigh(yearHigh);
+                BigDecimal yearLow = stock.getQuote().getYearLow();
+                stockInformation.setYearLow(yearLow);
+                BigDecimal oneYearTargetPrice = stock.getStats().getOneYearTargetPrice();
+                stockInformation.setTargetPrice(oneYearTargetPrice);
+
+                stockData.add(stockInformation);
+            });
+
+            BeanToCsv bc = new BeanToCsv();
+            ColumnPositionMappingStrategy mappingStrategy = new ColumnPositionMappingStrategy();
+
+            mappingStrategy.setType(StockInformation.class);
+
+            String[] columns = new String[]{"Id","stockCode","currentPrice","targetPrice", "yearHigh", "yearLow"};
+
+            mappingStrategy.setColumnMapping(columns);
+
+            bc.write(mappingStrategy,writer,stockData);
+            System.out.println("CSV File written successfully!!!");
+
+            writer.close();
+
+            model.addAttribute("message", "CSV File written successfully!!!");
+        }
+        catch(IOException ex) {
+            System.out.print("Error");
+        }
+    }
+
+}
