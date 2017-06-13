4f107d79b25906ae4294de5f764450f4b79c0b32
Tanvi Bhonsle <tanvi16.dalvi@gmail.com>
2017-06-13 12:07:29 +0530

    multithreading added

--- src/main/java/stocks/StocksController.java
+++ src/main/java/stocks/StocksController.java
@@ -20,7 +20,12 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 /**
@@ -32,8 +37,68 @@ public class StocksController {
 
     private static final Logger logger = LoggerFactory.getLogger(StocksController.class);
 
+//    @RequestMapping("/")
+//    public String stocksInformation() {
+//        try {
+//            // Read data from input text file
+//            Stream<String> streams = Files.lines(Paths.get("/Users/tanvi.bhonsle/Downloads/Stocks.txt"));
+//
+//            //Create csv for output data
+//            String csv = "/Users/tanvi.bhonsle/Downloads/output.csv";
+//
+//            CSVWriter writer = new CSVWriter(new FileWriter(csv));
+//            //List that stores the required data from the Yahoo API
+//            List<StockInformation> stockData = new ArrayList<>();
+//            final Integer[] idCounter = {1};
+//            streams.forEach(stream -> {
+//                StockInformation stockInformation = new StockInformation();
+//                stockInformation.setId(idCounter[0]++);
+//                stockInformation.setStockCode(stream);
+//                Stock stock = null;
+//                try {
+//                    stock = YahooFinance.get(stream);
+//                    logger.debug("finance data fetched for stock code: " + stream);
+//                } catch (IOException e) {
+//                    System.out.print("Error while fetching finance data");
+//                }
+//                BigDecimal price = stock.getQuote().getPrice();
+//                stockInformation.setCurrentPrice(price);
+//                BigDecimal yearHigh = stock.getQuote().getYearHigh();
+//                stockInformation.setYearHigh(yearHigh);
+//                BigDecimal yearLow = stock.getQuote().getYearLow();
+//                stockInformation.setYearLow(yearLow);
+//                BigDecimal oneYearTargetPrice = stock.getStats().getOneYearTargetPrice();
+//                stockInformation.setTargetPrice(oneYearTargetPrice);
+//
+//                stockData.add(stockInformation);
+//            });
+//
+//            BeanToCsv bc = new BeanToCsv();
+//            ColumnPositionMappingStrategy mappingStrategy = new ColumnPositionMappingStrategy();
+//
+//            mappingStrategy.setType(StockInformation.class);
+//
+//            String[] columns = new String[]{"Id","stockCode","currentPrice","targetPrice", "yearHigh", "yearLow"};
+//
+//            mappingStrategy.setColumnMapping(columns);
+//
+//            bc.write(mappingStrategy,writer,stockData);
+//            System.out.println("CSV File written successfully!!!");
+//            logger.debug("Welcome {}", "testing");
+//
+//            writer.close();
+//
+////            model.addAttribute("message", "CSV File written successfully!!!");
+//            return "CSV File written successfully!!!";
+//        }
+//        catch(IOException ex) {
+//            System.out.print("Error");
+//            return "Error";
+//        }
+//    }
+
     @RequestMapping("/")
-    public String stocksInformation() {
+    public String stocksInformation() throws InterruptedException {
         try {
             // Read data from input text file
             Stream<String> streams = Files.lines(Paths.get("/Users/tanvi.bhonsle/Downloads/Stocks.txt"));
@@ -44,29 +109,64 @@ public class StocksController {
             CSVWriter writer = new CSVWriter(new FileWriter(csv));
             //List that stores the required data from the Yahoo API
             List<StockInformation> stockData = new ArrayList<>();
-            final Integer[] i = {1};
-            streams.forEach(stream -> {
-                StockInformation stockInformation = new StockInformation();
-                stockInformation.setId(i[0]++);
-                stockInformation.setStockCode(stream);
-                Stock stock = null;
-                try {
-                    stock = YahooFinance.get(stream);
-                    logger.debug("finance data fetched for stock code: " + stream);
-                } catch (IOException e) {
-                    System.out.print("Error while fetching finance data");
-                }
-                BigDecimal price = stock.getQuote().getPrice();
-                stockInformation.setCurrentPrice(price);
-                BigDecimal yearHigh = stock.getQuote().getYearHigh();
-                stockInformation.setYearHigh(yearHigh);
-                BigDecimal yearLow = stock.getQuote().getYearLow();
-                stockInformation.setYearLow(yearLow);
-                BigDecimal oneYearTargetPrice = stock.getStats().getOneYearTargetPrice();
-                stockInformation.setTargetPrice(oneYearTargetPrice);
+            final Integer[] idCounter = {1};
 
-                stockData.add(stockInformation);
+            ExecutorService executor = Executors.newFixedThreadPool(10);
+            Collection callableList = new LinkedList<>();
+            streams.forEach(stream -> {
+                callableList.add(new Callable() {
+                    @Override
+                    public Boolean call() throws Exception {
+                        StockInformation stockInformation = new StockInformation();
+                        stockInformation.setId(idCounter[0]++);
+                        stockInformation.setStockCode(stream);
+                        Stock stock = null;
+                        try {
+                            stock = YahooFinance.get(stream);
+                            logger.debug("finance data fetched for stock code: " + stream);
+                        } catch (IOException e) {
+                            System.out.print("Error while fetching finance data");
+                        }
+                        BigDecimal price = stock.getQuote().getPrice();
+                        stockInformation.setCurrentPrice(price);
+                        BigDecimal yearHigh = stock.getQuote().getYearHigh();
+                        stockInformation.setYearHigh(yearHigh);
+                        BigDecimal yearLow = stock.getQuote().getYearLow();
+                        stockInformation.setYearLow(yearLow);
+                        BigDecimal oneYearTargetPrice = stock.getStats().getOneYearTargetPrice();
+                        stockInformation.setTargetPrice(oneYearTargetPrice);
+
+                        stockData.add(stockInformation);
+                        return true;
+                    }
+                });
             });
+            List futures = executor.invokeAll(callableList);
+
+            executor.shutdown();
+
+//            streams.forEach(stream -> {
+//                StockInformation stockInformation = new StockInformation();
+//                stockInformation.setId(idCounter[0]++);
+//                stockInformation.setStockCode(stream);
+//                Stock stock = null;
+//                try {
+//                    stock = YahooFinance.get(stream);
+//                    logger.debug("finance data fetched for stock code: " + stream);
+//                } catch (IOException e) {
+//                    System.out.print("Error while fetching finance data");
+//                }
+//                BigDecimal price = stock.getQuote().getPrice();
+//                stockInformation.setCurrentPrice(price);
+//                BigDecimal yearHigh = stock.getQuote().getYearHigh();
+//                stockInformation.setYearHigh(yearHigh);
+//                BigDecimal yearLow = stock.getQuote().getYearLow();
+//                stockInformation.setYearLow(yearLow);
+//                BigDecimal oneYearTargetPrice = stock.getStats().getOneYearTargetPrice();
+//                stockInformation.setTargetPrice(oneYearTargetPrice);
+//
+//                stockData.add(stockInformation);
+//            });
 
             BeanToCsv bc = new BeanToCsv();
             ColumnPositionMappingStrategy mappingStrategy = new ColumnPositionMappingStrategy();
@@ -89,6 +189,9 @@ public class StocksController {
         catch(IOException ex) {
             System.out.print("Error");
             return "Error";
+        } catch(Exception ex) {
+            System.out.print("Error");
+            return "Error in threading";
         }
     }
 
